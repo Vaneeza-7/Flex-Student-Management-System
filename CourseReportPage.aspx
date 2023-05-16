f8a2032d6e6ddcfcb797b9c5da308e8e990d89ac
Rabail-RN <120187350+Rabail-RN@users.noreply.github.com>
2023-05-17 01:02:16 +0500

    Report Page of Offered Courses

---
+++ CourseReportPage.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CourseReportPage.aspx.cs" Inherits="CourseReportPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <style type="text/css">
+        #form1 {
+            height: 776px;
+        }
+
+        .custom-table {
+            border-collapse: collapse;
+            font-family: Arial, sans-serif;
+            margin: auto;
+        }
+
+        .custom-table th, .custom-table td {
+            border: 1px solid #ccc;
+            padding: 8px;
+        }
+
+        .custom-table th {
+            background-color: dodgerblue;
+            text-align: left;
+            color: white;
+        }
+
+        .custom-table tr {
+            background-color: #f2f2f2;
+        }
+
+        .custom-table tr:hover {
+            background-color: #ddd;
+        }
+
+    </style>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="background-color:cornflowerblue; height: 68px;">
+            <h2 style="color:white; align-content:center; height: 57px; margin-left : 297px; width: 452px;margin-top:10px;">
+                Report of Offered Courses
+            </h2>
+        </div>
+
+        <div style="margin-top: 10px;border:medium double black; height: 679px;">
+            
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ CourseReportPage.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+public partial class CourseReportPage : System.Web.UI.Page
+{
+    DataTable Table;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Fetch data from the database using a select query
+        Table = DataForReport();
+
+        // Generate the report HTML dynamically
+        string report = ReportGeneration(Table);
+
+        // Save the report HTML to a file
+        string Path = Server.MapPath("~/Reports/CoursesOfferedReport.html");
+        SaveReport(report, Path);
+
+        // Display the report in the browser
+        Response.Clear();
+        Response.ContentType = "text/html";
+        Response.WriteFile(Path);
+        Response.End();
+    }
+
+    private DataTable DataForReport()
+    {
+        string connection = "Data Source=DESKTOP-DGFGQKN\\SQLEXPRESS;Initial Catalog=flex;Integrated Security=True";
+        using (SqlConnection con = new SqlConnection(connection))
+        {
+            string query = "SELECT offers.semester as 'Semester',offers.cId as 'Course Code',course.cname 'Course Name',course.crdHrs as 'Credit Hours' FROM offers Inner Join course ON offers.cId = course.code";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+
+    private string ReportGeneration(DataTable table)
+    {
+        StringWriter stringWriter = new StringWriter();
+        using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter))
+        {
+
+            writer.AddStyleAttribute("border-collapse", "collapse");
+            writer.AddStyleAttribute("font-family", "Arial, sans-serif");
+            writer.AddStyleAttribute("margin", "auto");
+            writer.RenderBeginTag(HtmlTextWriterTag.Table);
+
+            
+
+            // Generate the table header row
+            writer.RenderBeginTag(HtmlTextWriterTag.Thead);
+            writer.RenderBeginTag(HtmlTextWriterTag.Tr);
+            for (int i = 0; i <Table.Columns.Count;i++)
+            {
+                DataColumn col = Table.Columns[i];
+                writer.AddStyleAttribute("background-color", "dodgerblue");
+                writer.AddStyleAttribute("padding", "8px");
+                writer.AddStyleAttribute("color", "White");
+                writer.RenderBeginTag(HtmlTextWriterTag.Th);
+                writer.Write(col.ColumnName);
+                writer.RenderEndTag(); 
+            }
+            writer.RenderEndTag(); // </tr>
+            writer.RenderEndTag(); // </thead>
+
+            // Generate the table body rows
+            writer.RenderBeginTag(HtmlTextWriterTag.Tbody);
+            for (int i =0;i<Table.Rows.Count;i++)
+            {
+                writer.RenderBeginTag(HtmlTextWriterTag.Tr);
+                DataRow row = Table.Rows[i];
+                for (int j =0; j<row.ItemArray.Length;j++)
+                {
+                    object item = row.ItemArray[j];
+                    writer.AddStyleAttribute("border", "1px solid #ccc");
+                    writer.AddStyleAttribute("padding", "8px");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Td);
+                    writer.Write(item.ToString());
+                    writer.RenderEndTag();
+                }
+                writer.RenderEndTag();
+            }
+            writer.RenderEndTag();
+
+            writer.RenderEndTag();
+
+        }
+        return stringWriter.ToString();
+    }
+
+    private void SaveReport(string rep,string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            writer.Write(rep);
+        }
+    }
+}
